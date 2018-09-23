8ff468b704f9f1d012131e7697c057e83f0b25b9
Karan Gurazada <karangurazada@gmail.com>
2018-09-23 06:12:03 -0700

    dpofigjosidfjg

--- medscore.py
+++ medscore.py
@@ -5,5 +5,5 @@ analyser = SentimentIntensityAnalyzer()
 s = sys.argv[1]
 snt = analyser.polarity_scores(s)
 print(snt)
-sys.stderr.write(String(snt))
+sys.stderr.write(str(snt))
 sys.stderr.write("finished medscore!\n")
