f75b5a462dbc8c22e8bc9ef86ba4de9a1ca085b5
Siddharth Srinivasan <33235342+sidsrini@users.noreply.github.com>
2018-09-22 20:43:06 -0700

    Update medscore

--- medscore.py
+++ medscore.py
@@ -1,6 +1,6 @@
 from vaderSentiment import sentiment as vaderSentiment 
 import json
-jsonFile = open('your_filename.json', 'r')
+jsonFile = open('user_report.json', 'r')
 sentences = json.load(jsonFile)
 for p in sentences['symptom']:
     sentiment = vaderSentiment(p)
