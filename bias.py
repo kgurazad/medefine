7bc393810b73a91474280d92caf077658066a2e3
Siddharth Srinivasan <33235342+sidsrini@users.noreply.github.com>
2018-09-22 21:56:15 -0700

    fuq u karan

--- bias.py
+++ bias.py
@@ -1,4 +1,5 @@
 import sys
+from scipy import stats
 s = []
 for x in range(1,len(sys.argv)):
     s.append(sys.argv[x])
@@ -9,3 +10,12 @@ for x in s:
     sum = sum + x
 
 print(sum/(len(sys.argv)-1))
+
+c = []
+
+for x in range(1, len(sys.argv)):
+    c.append(x)
+
+slope, intercept, r_value, p_value, std_err= stats.linregress(c,s)
+
+
