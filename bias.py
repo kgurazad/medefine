f451dff57cb6d20f8bfa459d111b8f373c0d5a23
Karan Gurazada <karangurazada@gmail.com>
2018-09-23 05:40:19 -0700

    dpofigjosidfjg

--- bias.py
+++ bias.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from sklearn import datasets, linear_model
 from sklearn.metrics import mean_squared_error, r2_score
+sys.stderr.write("started bias!\n")
 s = []
 for x in range(1,len(sys.argv)):
     s.append(sys.argv[x])
@@ -16,7 +17,7 @@ for x in s:
     sum = sum + x
 
 print(sum/(len(sys.argv)-1))
-
+sys.stderr.write("finished bias!\n")
 c = []
 
 for x in range(1, len(sys.argv)):
--- medscore.py
+++ medscore.py
@@ -1,6 +1,8 @@
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 import sys
+sys.stderr.write("started medscore!\n")
 analyser = SentimentIntensityAnalyzer()
 s = sys.argv[1]
 snt = analyser.polarity_scores(s)
 print(snt)
+sys.stderr.write("finished medscore!\n")
