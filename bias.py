500fc7e8336e7c257a13cade2a7d42e48a2619ac
Karan Gurazada <karangurazada@gmail.com>
2018-09-23 06:26:47 -0700

    dpofigjosidfjg

--- bias.py
+++ bias.py
@@ -19,19 +19,3 @@ for x in s:
 print(sum/(len(sys.argv)-1))
 sys.stderr.write(str(sum/(len(sys.argv)-1)) + " <- bias")
 sys.stderr.write("finished bias!\n")
-c = []
-
-for x in range(1, len(sys.argv)):
-    c.append(x)
-
-slope, intercept, r_value, p_value, std_err= stats.linregress(c,s)
-
-def fit_func(x, a, b, c, d):
-    return a*x*x*x+b*x*x+c*x+d
-params = curve_fit(fit_func, c, s)
-[a, b, c, d, e, f, g, h, i, j, k, l, m] = params[0]
-def predict_next(a, b, c, d):
-    x = len(sys.argv)
-    return a*x*x*x+b*x*x+c*x+d
-plt.plot(c,s)
-savefig('foo.jpg')
