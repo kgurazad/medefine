22751330af21e31834379682e4ac301b237fe751
Siddharth Srinivasan <33235342+sidsrini@users.noreply.github.com>
2018-09-22 21:11:38 -0700

    Update medscore.py

--- medscore.py
+++ medscore.py
@@ -4,7 +4,4 @@ jsonFile = open('user_report.json', 'r')
 sentences = json.load(jsonFile)
 for p in sentences['symptom']:
     sentiment = vaderSentiment(p)
-    with open('scores.json', 'r' as f):
-        s = json.load(f)
-        s.append(sentiment)
-        json.dump(s,f)
+    print(sentiment)
